b7c18903a297de2657b6467e9792e9b861f7a87b
yural64 <yural64@gmail.com>
2024-09-19 16:57:10 +0300

    edit class User

--- dzOB02.py
+++ dzOB02.py
@@ -1,13 +1,30 @@
-class User:
-    def __init__(self, user_id, user_name, user_access):
-        self.user_id = user_id
-        self.user_name = user_name
-        self.user_access = user_access
+
+
+class User():
+    def __init__(self, user_id, user_name):
+        self.__user_id = user_id
+        self.__user_name = user_name
+        self.__user_access = "user"
+        self.users = []
+
+
+def get_user_id(self):
+    return self.__user_id
+
+def get_user_name(self):
+    return self.__user_name
+
+def get_user_access(self):
+    return self.__user_access
+
+def set_user_name(self, user_name):
+    self.__user_name - user_name
+
 
 
 class Admin(User):
-    def __init__(self, user_id, user_name, user_access, admin_access):
-        super().__init__(user_id, user_name, user_access)
+    def __init__(self, user_id, user_name, admin_access):
+        super().__init__(user_id, user_name)
         self.admin_access = admin_access
 
     def add_user(self):
